0a9d24b04597aa67025ac6b43004594d9c817dc3
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-16 19:58:45 +0300

    menu update20

--- application.php
+++ application.php
@@ -55,8 +55,9 @@ get_header();
 					<?php
 					$menu_name = "application";
 					$menu_items = wp_get_nav_menu_items( $menu_name );
+					$menu_list='';
 					foreach ( (array) $menu_items as $key => $menu_item ){
-						$menu_list='<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center">';
+						$menu_list.='<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center">';
                         $menu_list .= '<a href="' . $menu_item->url . '"><img src="' . the_field('term_image', 'tag_'.$menu_item->id) . '"></a>';
 						$menu_list.='</div>';
 					}
