1c939f7df286dbf6bbb4090087e8c089722fa603
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-16 22:31:08 +0300

    fixing product page1

--- products.php
+++ products.php
@@ -10,40 +10,50 @@
  *
  * */
 get_header();
-?>
+if ( have_posts() ) :
+	while (have_posts()) {
+		the_post();
+		?>
 
-<main>
-	<div class="container-fluid product-range">
-		<div class="row">
-			<?php
-			$args = array(
-                'category_name' => 'product,product_master'
-                );
-			query_posts($args);
-			if ( have_posts() ) :
-			while (have_posts()) { the_post();    ?>
+        <main>
+            <div class="container-fluid product-range">
+    <div class="row"><img src="<?php the_post_thumbnail_url();  ?>" alt=""></div>
+                <div class="row">
+					<?php
+					$args = [
+						'category_name' => 'product,product_master'
+					];
+					query_posts($args);
+					if (have_posts()) :
+						while (have_posts()) {
+							the_post(); ?>
 
-			<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 product-item text-center">
-				<img src="<?php the_post_thumbnail_url(  );?>" height="335" width="299" alt="" class="product-img">
-				<div class="product-info">
-					<p class="product-title">
-						<?php the_title();?>
-					</p>
-					<a class="link d-inline-flex align-items-center font-size-auto" href="<?php the_permalink()?>">
-						ПОДРОБНЕЕ
-						<i data-feather="arrow-right"></i>
-					</a>
-				</div>
-			</div>
-			<?php
-            };
-            endif;
-            wp_reset_query();
-            ?>
+                            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 product-item text-center">
+                                <a href="<?php the_permalink() ?>">
+                                <div class="product-info">
+                                    <p class="product-title">
+										<?php the_title(); ?>
+                                    </p>
+                                    <!--a class="link d-inline-flex align-items-center font-size-auto"
+                                       href="<?php the_permalink() ?>">
+                                        ПОДРОБНЕЕ
+                                        <i data-feather="arrow-right"></i>
+                                    </a-->
+                                </div>
+                                <img src="<?php the_post_thumbnail_url(); ?>" height="335" width="299" alt=""
+                                     class="product-img">
+                                </a>
+                            </div>
+							<?php
+						};
+					endif;
+					wp_reset_query();
+					?>
 
-		</div>
-	</div>
-</main>
-<?php
-get_footer();
+                </div>
+            </div>
+        </main>
+		<?php
+	};endif;
+	get_footer();
 ?>
