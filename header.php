ca6207446b09aef8d7043c007bb7c9b6b497ef00
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-17 00:05:25 +0300

    fixing header2

--- header.php
+++ header.php
@@ -40,13 +40,13 @@
 		            if (get_page_link() != $menu_item->url) {
 			            $menu_list .= '<li class="nav-item">';
 			            $menu_list .= '<div class="nav_item_l"></div>';
-                        $menu_list .= '<a class="nav-link" href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
+                        $menu_list .= '<a class="nav-link" href="' . $menu_item->url . '">' . $menu_item->title . '</a>';
 			            $menu_list .= '<div class="nav_item_r"></div>';
 			            $menu_list .= '</li>';
 		            } else {
 		                $menu_list .= '<li class="nav-item active">';
 			            $menu_list .= '<div class="nav_item_l"></div>';
-			            $menu_list .= '<a class="nav-link" href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
+			            $menu_list .= '<a class="nav-link" href="' . $menu_item->url . '">' . $menu_item->title . '</a>';
 			            $menu_list .= '<div class="nav_item_r"></div>';
 			            $menu_list .= '</li>';
 		            }
