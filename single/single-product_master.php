665ef454d271e15f6ca9981ba8c537e60f41272b
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-17 14:58:30 +0300

    product master update6

--- single/single-product_master.php
+++ single/single-product_master.php
@@ -37,8 +37,8 @@ if ( have_posts() ) :
 						while (have_posts()) {
 
 							the_post();
-
-							if (get_field("master")==$master_id){
+                            echo get_field("master")." ".$master_id;
+							//if (get_field("master")==$master_id){
 							?>
 
                             <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 product-item text-center">
@@ -56,7 +56,7 @@ if ( have_posts() ) :
                                 </div>
                             </div>
 							<?php
-							};
+							//};
 						};
 					endif;
 					wp_reset_query();
