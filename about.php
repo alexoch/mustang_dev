21df27952b5cbe30edd801efbeaf3283b7b1907b
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-18 10:16:46 +0300

    new history3

--- about.php
+++ about.php
@@ -71,69 +71,16 @@ while (have_posts()) {
             <div class="row about-graph border-bt border-tp">
 
                 <div class="timeline">
-                    <div class="year">
-                        <div class="inner">
-                            <span>2016</span>
+                    <div class="container left">
+                        <div class="content">
+                            <h2>2017</h2>
+                            <p>Lorem ipsum..</p>
                         </div>
                     </div>
-
-                    <ul class="days">
-                        <li class="day">
-                            <div class="events">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
-                                <div class="date">18 October (Monday)</div>
-                            </div>
-                        </li>
-
-                        <li class="day">
-                            <div class="events">
-                                <p>Lorem dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
-                                <div class="date">18 October (Monday)</div>
-                            </div>
-                        </li>
-
-                        <li class="day">
-                            <div class="events">
-                                <div class="day__img">
-                                    <img src="http://placehold.it/400x300" alt="" />
-                                    <p class="caption">
-                                        Lorem ipsum dolor sit amet.
-                                    </p>
-                                </div>
-                                <div class="date">18 October (Monday)</div>
-                            </div>
-                        </li>
-
-                        <li class="day">
-                            <div class="events">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
-                                <div class="date">18 October (Monday)</div>
-                            </div>
-                        </li>
-
-                        <li class="day">
-                            <div class="events">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
-                                <div class="date">18 October (Monday)</div>
-                            </div>
-                        </li>
-
-                        <li class="day">
-                            <div class="events">
-                                <div class="day__img">
-                                    <img src="http://placehold.it/400x300" alt="" />
-                                    <p class="caption">
-                                        Lorem ipsum dolor sit amet.
-                                    </p>
-                                </div>
-                                <div class="date">18 October (Monday)</div>
-                            </div>
-                        </li>
-                    </ul>
-
-                    <div class="year year--end">
-                        <div class="inner">
-                            <span>2017</span>
+                    <div class="container right">
+                        <div class="content">
+                            <h2>2016</h2>
+                            <p>Lorem ipsum..</p>
                         </div>
                     </div>
                 </div>
