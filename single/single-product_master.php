71bd30c6575b9bc1e39be9f7be99b69fe0036b59
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-24 13:19:46 +0300

    fixing master product7

--- single/single-product_master.php
+++ single/single-product_master.php
@@ -27,7 +27,7 @@ if (have_posts()) :
                 </div>
                 <div class="row ">
 					<?php
-					$posts = get_posts('category_name=product_sub&order=DSC');
+					$posts = get_posts('category_name=product_sub&order=DSC&posts_per_page=-1');
 					foreach ($posts as $post) {
 						setup_postdata($post);
 						var_dump(get_field("master")[0]);
