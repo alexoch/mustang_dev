612cf5f35ce775b1686ce9515f60c72b4dba7d0f
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-31 10:29:55 +0200

    trading equipment

--- trading_equipment.php
+++ trading_equipment.php
@@ -13,40 +13,60 @@
 get_header();
 ?>
 <main>
-	<div class="container-fluid border-tp">
-		<div class="row">
-			<div class="container col-12 mt-4 mb-4">
-				<h5 class="font-weight-bold mb-0 text-uppercase">
-					Торговое оборудование
-				</h5>
-			</div>
-		</div>
-	</div>
+    <div class="container-fluid border-tp">
+        <div class="row">
+            <div class="container col-12 mt-4 mb-4">
+                <h5 class="font-weight-bold mb-0 text-uppercase">
+                    Торговое оборудование
+                </h5>
+            </div>
+        </div>
+    </div>
 
-	<div class="container-fluid product-range">
-		<div class="row">
+    <div class="container-fluid product-range">
+        <div class="row">
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 product-item text-center product-big">
+                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg" height="335"
+                     width="299" alt="" class="product-img">
+                <div class="product-info">
+                    <p class="product-title">
+                        Стеллаж #1
+                    </p>
+                </div>
+            </a>
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 product-item text-center product-big">
+                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg" height="335"
+                     width="299" alt="" class="product-img">
+                <div class="product-info">
+                    <p class="product-title">
+                        Стеллаж #2
+                    </p>
+                </div>
+            </a>
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 product-item text-center product-big">
+                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg" height="335"
+                     width="299" alt="" class="product-img">
+                <div class="product-info">
+                    <p class="product-title">
+                        Стеллаж #3
+                    </p>
+                </div>
+            </a>
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 product-item text-center product-big">
+                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg" height="335"
+                     width="299" alt="" class="product-img">
+                <div class="product-info">
+                    <p class="product-title">
+                        Стеллаж #4
+                    </p>
+                </div>
+            </a>
 
-		<?php
-
-		query_posts('category_name=product&order=DSC');
-
-
-		while (have_posts()) {
-		the_post();
-		?>
-			<a data-fancybox="gallery" href="img/trading equipment/1.jpg" class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 product-item text-center product-big">
-				<img src="img/trading equipment/1.jpg" height="335" width="299" alt="" class="product-img">
-				<div class="product-info">
-					<p class="product-title">
-						Стеллаж #1
-					</p>
-				</div>
-			</a>
-
-
-		<?php }
-		?></div>
-	</div>
+        </div>
 </main>
 
 <?php
