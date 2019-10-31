27881b281fe3aca113f0f28d442257959429d64e
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-31 12:18:24 +0200

    trading equipment8

--- trading_equipment.php
+++ trading_equipment.php
@@ -56,15 +56,27 @@ get_header();
 
                 <div class="trade_product-info">
                     <p class="trade_product-title">
-                        Стеллаж #3
+                        Стеллаж #2
                     </p>
                 </div>
             </a>
             <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
                class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div class="product-img">
+                <div class="trade_product-img">
                     <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg" height="335"
                           width="299" alt="" >
+                </div>
+                <div class="trade_product-info">
+                    <p class="trade_product-title">
+                        Стеллаж #3
+                    </p>
+                </div>
+            </a>
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
+                <div class="trade_product-img">
+                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg" height="335"
+                         width="299" alt="" >
                 </div>
                 <div class="trade_product-info">
                     <p class="trade_product-title">
@@ -72,7 +84,6 @@ get_header();
                     </p>
                 </div>
             </a>
-
         </div>
 </main>
 
