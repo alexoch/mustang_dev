5a7581eeb6323aba8e6cb58fb682500e466e0f66
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-31 12:10:41 +0200

    trading equipment6

--- trading_equipment.php
+++ trading_equipment.php
@@ -27,7 +27,7 @@ get_header();
         <div class="row">
             <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
                class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div class="product-img">
+                <div class="trade_product-img">
                     <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg" height="335"
                          width="299" alt="" >
                 </div>
