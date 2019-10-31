599bbade221b52ac3e38f29fec7114f68969490b
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-31 20:51:32 +0200

    trading equipment

--- trading_equipment.php
+++ trading_equipment.php
@@ -25,62 +25,54 @@ get_header();
 
     <div class="container-fluid product-range">
         <div class="row">
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div class="trade_product-img">
-                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg" height="335"
-                         width="299" alt="" >
-                </div>
-                <div class="trade_product-info">
-                    <p class="trade_product-title">
-                        Стеллаж #1
-                    </p>
-                </div>
-            </a>
-            <!--a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 product-item text-center product-big">
-                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg" height="335"
-                     width="299" alt="" class="product-img">
-                <div class="product-info">
-                    <p class="product-title">
-                        Стеллаж #2
-                    </p>
-                </div>
-            </a-->
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div class="trade_product-img">
-                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg" height="335"
-                     width="299" alt="" >
-                </div>
 
-                <div class="trade_product-info">
-                    <p class="trade_product-title">
-                        Стеллаж #2
-                    </p>
-                </div>
-            </a>
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div class="trade_product-img">
-                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg" height="335"
-                          width="299" alt="" >
-                </div>
-                <div class="trade_product-info">
-                    <p class="trade_product-title">
-                        Стеллаж #3
-                    </p>
-                </div>
-            </a>
             <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center trade_product">
                 <div class="trade_product-img">
                     <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg" height="335"
                          width="299" alt="" >
                 </div>
                 <div class="trade_product-info">
                     <p class="trade_product-title">
-                        Стеллаж #4
+                        Арт. MS01
+                    </p>
+                </div>
+            </a>
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center trade_product">
+                <div class="trade_product-img">
+                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg" height="335"
+                          width="299" alt="" >
+                </div>
+                <div class="trade_product-info">
+                    <p class="trade_product-title">
+                        Арт. MS02
+                    </p>
+                </div>
+            </a>
+
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center trade_product">
+                <div class="trade_product-img">
+                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg" height="335"
+                         width="299" alt="" >
+                </div>
+
+                <div class="trade_product-info">
+                    <p class="trade_product-title">
+                        Арт. MS03
+                    </p>
+                </div>
+            </a>
+            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
+               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center trade_product">
+                <div class="trade_product-img">
+                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg" height="335"
+                         width="299" alt="" >
+                </div>
+                <div class="trade_product-info">
+                    <p class="trade_product-title">
+                        Арт. MD01
                     </p>
                 </div>
             </a>
