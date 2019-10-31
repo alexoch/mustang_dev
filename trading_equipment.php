974cb1af035a3edd4a47ab42ffeb5c556aad3c72
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-10-31 12:07:40 +0200

    trading equipment5

--- trading_equipment.php
+++ trading_equipment.php
@@ -27,8 +27,10 @@ get_header();
         <div class="row">
             <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
                class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg" height="335"
-                     width="299" alt="" class="product-img">
+                <div class="product-img">
+                    <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg" height="335"
+                         width="299" alt="" >
+                </div>
                 <div class="product-info">
                     <p class="product-title">
                         Стеллаж #1
@@ -47,11 +49,9 @@ get_header();
             </a-->
             <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg"
                class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div style="position: relative;">
+                <div class="trade_product-img">
                 <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg" height="335"
-                     width="299" alt="" class="product-img" style=" position: absolute;
-  top: 50%;
-  transform: translateY(-50%);">
+                     width="299" alt="" >
                 </div>
 
                 <div class="trade_product-info">
@@ -62,9 +62,9 @@ get_header();
             </a>
             <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
                class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 text-center trade_product">
-                <div style="">
+                <div class="product-img">
                     <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg" height="335"
-                          width="299" alt="" class="product-img">
+                          width="299" alt="" >
                 </div>
                 <div class="trade_product-info">
                     <p class="trade_product-title">
