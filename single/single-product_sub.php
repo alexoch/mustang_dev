0034b361aed4ac898ddb10041bcb01d4e47f3270
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-11-06 19:18:17 +0200

    single update3

--- single/single-product_sub.php
+++ single/single-product_sub.php
@@ -20,6 +20,7 @@ get_header();
                 <h1 class="product__title">
 					<?php the_title(); ?>
                 </h1>
+                <?php the_field("galereya");?>
                 <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="product__present">
             </div>
             <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6   text-center">
