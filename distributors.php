06749d8dde723ddb667929e982a90fcf67b382dc
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-11-19 12:09:53 +0200

    fixing product pages11

--- distributors.php
+++ distributors.php
@@ -25,23 +25,23 @@ get_header();
 				</h6>
 			</a>
 			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="http://mustang.sportquest.com.ua/souvenirs/">
-				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/tshirt.png" alt="" class="distributors-img">
-				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/tshirt.png" class="distributors-img-hover" />
+				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/souvenir.png" alt="" class="distributors-img">
+				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/souvenir.png" class="distributors-img-hover" />
 				<h6 class="distributors-title">
 					Рекламно - сувенирная <br> продукция
 				</h6>
 			</a>
 			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="http://mustang.sportquest.com.ua/trading-equipment/">
-				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/shelf.png" alt="" class="distributors-img">
-				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/shelf.png" class="distributors-img-hover" />
+				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/equipment.png" alt="" class="distributors-img">
+				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/equipment.png" class="distributors-img-hover" />
 				<h6 class="distributors-title">
 					Торговое <br> оборудование
 				</h6>
 			</a>
 			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="http://mustang.sportquest.com.ua/sales">
-				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/newspaper.png" alt="" class="distributors-img" />
-				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/newspaper.png" alt="" class="distributors-img-hover" />
-				<h6 class="distributors-title">
+				<img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/stock.png" alt="" class="distributors-img-hover" />
+                <img src="http://mustang.sportquest.com.ua/wp-content/themes/mustang/img/distributors/stock.png" alt="" class="distributors-img" />
+                <h6 class="distributors-title">
 					Акции
 				</h6>
 			</a>
