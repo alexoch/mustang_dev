5171ac673e782e183f6770357cd5d290f43aea3b
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-12-03 13:07:04 +0200

    fixes product application42

--- ad-products.php
+++ ad-products.php
@@ -39,7 +39,7 @@ get_header();
 					the_post(); ?>
                     <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
                         <div class="ad-product">
-                            <div class="ad-product-pic" style="background-image:url("<?php the_post_thumbnail_url()?>"); background-size:cover;">
+                            <div class="ad-product-pic" style="background-image:url('<?php the_post_thumbnail_url();?>'); background-size:cover;">
                                 <img src="<?php the_post_thumbnail_url(); ?>" height="335" width="299" alt=""
                                       class="ad-product-img" style="display: none;">
                             </div>
