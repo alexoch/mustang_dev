4be19ee0e00a1ffab4e6d46e5f724c502bbb3d65
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-12-05 11:44:04 +0200

    fixes single6

--- single/single.php
+++ single/single.php
@@ -89,38 +89,39 @@ get_header();
                     Скачать документы
                 </h3>
             </div>
-            <div class="row col-12 col-xl-8 col-xl-offset-2 text-center product_docs">
-                <?php
-                $file = get_field('pasport');
-                if( $file ):
-                // vars
-                    $url1 = $file['url'];
-                    $title = $file['title'];
+			<?php
+			$file = get_field('pasport');
+			if ($file):
+				// vars
+				$url1 = $file['url'];
+				$title = $file['title'];
 
-                endif;
-                $file = get_field('pasport_bezopasnosti');
-                if( $file ):
-	                // vars
-	                $url2 = $file['url'];
-	                $title = $file['title'];
+			endif;
+			$file = get_field('pasport_bezopasnosti');
+			if ($file):
+				// vars
+				$url2 = $file['url'];
+				$title = $file['title'];
 
-                endif;?>
+			endif; ?>
 
-                <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 product-item text-center">
-                    <a href="<?php echo $url1;?>" style="color: white;">
-                        <div>ПАСПОРТ ПРОДУКТА</div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1200px-PDF_file_icon.svg.png" alt="" style="width: 50px;margin: 10px 0;">
-                        <div>скачать</div>
-                    </a>
-                </div>
-                <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 product-item text-center col-xl-offset-2">
-                    <a href="<?php echo $url2;?>" style="color: white;">
-                        <div>ПАСПОРТ БЕЗОПАСНОСТИ</div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1200px-PDF_file_icon.svg.png" alt="" style="width: 50px;margin: 10px 0;">
-                        <div>скачать</div>
-                    </a>
-                </div>
+            <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3 product-item text-center col-xl-offset-2">
+                <a href="<?php echo $url1; ?>" style="color: white;">
+                    <div>ПАСПОРТ ПРОДУКТА</div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1200px-PDF_file_icon.svg.png"
+                         alt="" style="width: 50px;margin: 10px 0;">
+                    <div>скачать</div>
+                </a>
             </div>
+            <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3 product-item text-center col-xl-offset-1 ">
+                <a href="<?php echo $url2; ?>" style="color: white;">
+                    <div>ПАСПОРТ БЕЗОПАСНОСТИ</div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1200px-PDF_file_icon.svg.png"
+                         alt="" style="width: 50px;margin: 10px 0;">
+                    <div>скачать</div>
+                </a>
+            </div>
+
         </div>
     </div>
 	<?php }
