c452437045ce704568adedfbd1aea89a47211961
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-12-10 09:54:02 +0200

    fixes trading equipment

--- trading_equipment.php
+++ trading_equipment.php
@@ -20,70 +20,33 @@ get_header();
     </div>
     <div class="container-fluid product-range">
         <div class="row">
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center ">
-                <div class="trade_product">
-                    <div class="trade_product-img">
-                        <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0196-1.jpg"
-                             height="335"
-                             width="299" alt="" class="smallimg">
+	        <?php
+	        query_posts('category_name=stands&order=DSC');
+	        if (have_posts()) :
+	        while (have_posts()) {
+		        the_post(); ?>
+                <a data-fancybox="gallery"
+                   href="<?php the_post_thumbnail_url();?>"
+                   class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center ">
+                    <div class="trade_product">
+                        <div class="trade_product-img">
+                            <img src="<?php the_post_thumbnail_url();?>"
+                                 height="335"
+                                 width="299" alt="" class="smallimg">
+                        </div>
+                        <!--div class="trade_product-info">
+							<p class="trade_product-title">
+								Арт. MS01
+							</p>
+						</div-->
                     </div>
-                    <div class="trade_product-info">
-                        <p class="trade_product-title">
-                            Арт. MS01
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center ">
-                <div class="trade_product">
+                </a>
+		        <?php
+	        };
+            endif;
+            wp_reset_query();
+            ?>
 
-                    <div class="trade_product-img">
-                        <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0102-1.jpg"
-                             height="335"
-                             width="299" alt="">
-                    </div>
-                    <div class="trade_product-info">
-                        <p class="trade_product-title">
-                            Арт. MS02
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center ">
-                <div class="trade_product">
-
-                    <div class="trade_product-img">
-                        <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0123-1.jpg"
-                             height="335"
-                             width="299" alt="">
-                    </div>
-                    <div class="trade_product-info">
-                        <p class="trade_product-title">
-                            Арт. MS03
-                        </p>
-                    </div>
-                </div>
-            </a>
-            <a data-fancybox="gallery" href="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
-               class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center ">
-                <div class="trade_product">
-
-                    <div class="trade_product-img">
-                        <img src="http://mustang.sportquest.com.ua/wp-content/uploads/2019/10/dsc_0221-1.jpg"
-                             height="335"
-                             width="299" alt="">
-                    </div>
-                    <div class="trade_product-info">
-                        <p class="trade_product-title">
-                            Арт. MD01
-                        </p>
-                    </div>
-                </div>
-            </a>
-        </div>
 </main>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"/>
 <?php
