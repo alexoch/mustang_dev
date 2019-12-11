a4cc961cb366ed3682a655a8c6285bb081bd4fc6
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2019-12-11 15:47:47 +0200

    fixes trading equipment4

--- trading_equipment.php
+++ trading_equipment.php
@@ -9,7 +9,7 @@
 get_header();
 ?>
 <main>
-    <div class="container-fluid border-tp">
+    <!--div class="container-fluid border-tp">
         <div class="row">
             <div class="container col-12 mt-4 mb-4">
                 <h5 class="font-weight-bold mb-0 text-uppercase">
@@ -17,7 +17,7 @@ get_header();
                 </h5>
             </div>
         </div>
-    </div>
+    </div-->
     <div class="container-fluid product-range">
         <div class="row">
 	        <?php
@@ -27,7 +27,7 @@ get_header();
 		        the_post(); ?>
                 <a data-fancybox="gallery"
                    href="<?php the_post_thumbnail_url();?>"
-                   class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center ">
+                   class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 text-center distributors-item">
                     <div class="trade_product">
                         <div class="trade_product-img">
                             <img src="<?php the_post_thumbnail_url();?>"
