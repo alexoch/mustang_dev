95d0a78238b410d7dd8498ff5d8ec64ec17ff69b
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2020-01-14 13:37:02 +0200

    added latest16

--- tag.php
+++ tag.php
@@ -7,70 +7,75 @@
  */
 
 get_header();
-        ?>
-        <main>
-            <div class="container-fluid use">
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 submenu">
-                        <h5 class="submenu-title text-center">
-                            ПРИМЕНЕНИЕ
-                        </h5>
+?>
+    <main>
+        <div class="container-fluid use">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 submenu">
+                    <h5 class="submenu-title text-center">
+                        ПРИМЕНЕНИЕ
+                    </h5>
 
-                        <div class="submenu-nav">
-                                <?php
-                                $menu_name = "application";
-                                $menu_items = wp_get_nav_menu_items( $menu_name );
+                    <div class="submenu-nav">
+						<?php
+						$menu_name = "application";
+						$menu_items = wp_get_nav_menu_items($menu_name);
 
-                                $menu_list = '<ul id="menu-' . $menu_name . '">';
-                                foreach ( (array) $menu_items as $key => $menu_item ){
-	                                if(get_page_link()!=$menu_item->url ){
-		                                $menu_list .= '<li><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
-                                    }else{
-		                                $menu_list .= '<li class="use-active"><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
-                                    }
-                                }
-                                $menu_list .= '</ul>';
-                                echo $menu_list;
-                                ?>
+						$menu_list = '<ul id="menu-' . $menu_name . '">';
+						foreach ((array)$menu_items as $key => $menu_item) {
+							if (get_page_link() != $menu_item->url) {
+								$menu_list .= '<li><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
+							} else {
+								$menu_list .= '<li class="use-active"><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
+							}
+						}
+						$menu_list .= '</ul>';
+						echo $menu_list;
+						?>
 
-                            <form class="submenu-search" method="GET" action="<?php echo home_url(); ?>/search">
-                                <button id="subsearch">
-                                    <i data-feather="search"></i>
-                                </button>
-                                <input type="text" id="subsearch" name="s" placeholder="поиск по применению"/>
-                            </form>
-                        </div>
+                        <form class="submenu-search" method="GET" action="<?php echo home_url(); ?>/search">
+                            <button id="subsearch">
+                                <i data-feather="search"></i>
+                            </button>
+                            <input type="text" id="subsearch" name="s" placeholder="поиск по применению"/>
+                        </form>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9 ">
-                        <div class="container-fluid product-range">
-                            <div class="row">
-                            <?php
-                            /* Start the Loop */
-                            while ( have_posts() ) :
-                                the_post();
-                            ?>
-                                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 product-item text-center">
-                                        <a href="<?php the_permalink() ?>">
-                                            <div class="product-info">
-                                                <p class="product-title">
-                                                    <?php the_title(); ?>
-                                                </p>
-                                            </div>
-                                            <img src="<?php the_post_thumbnail_url(); ?>" height="335" width="299" alt=""
-                                                 class="product-img">
-                                        </a>
-                                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9 ">
+                    <div class="container-fluid product-range">
+                        <div class="row">
+							<?php
+							/* Start the Loop */
+							while (have_posts()) :
+								the_post();
+								?>
+                                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 product-item text-center">
+									<?php if (has_tag("latest")): ?>
+                                        <span class="new_prod">
+                                    new
+                                </span>
+									<?php endif; ?>
+                                    <a href="<?php the_permalink() ?>">
+                                        <div class="product-info">
+                                            <p class="product-title">
+												<?php the_title(); ?>
+                                            </p>
+                                        </div>
+                                        <img src="<?php the_post_thumbnail_url(); ?>" height="335" width="299" alt=""
+                                             class="product-img">
+                                    </a>
+                                </div>
 
 
-                            <?php endwhile;?>
-                            </div>
+							<?php endwhile; ?>
                         </div>
                     </div>
                 </div>
             </div>
-        </main>
+        </div>
+    </main>
 
-		<?php
+<?php
 
 get_footer();
 
