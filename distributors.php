5eb55da78ef62dd2f8154b4169257ed58564debb
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2020-01-27 14:18:44 +0200

    updating distributors

--- distributors.php
+++ distributors.php
@@ -17,34 +17,27 @@ get_header();
 <main>
 	<div class="container-fluid distributors">
 		<div class="row">
-			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="<?php echo home_url(); ?>/wp-content/uploads/2019/11/catalog-mustang.pdf">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/pdf.png" alt="" class="distributors-img">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/pdf.png" class="distributors-img-hover" />
+            <?php
+            $args = [
+	            'category_name'  => 'distributors',
+	            'posts_per_page' => -1,
+	            'order'          => "ASC"
+            ];
+            query_posts($args);
+            if (have_posts()) :
+            while (have_posts()) {
+            the_post(); ?>
+
+            <a class="col-12 col-sm-12 col-md-12 col-lg-12 col- xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="<?php the_field("address")?>">
+				<img src="<?php the_post_thumbnail_url(); ?>" alt="" class="distributors-img">
+				<img src="<?php the_post_thumbnail_url(); ?>" class="distributors-img-hover" />
 				<h6 class="distributors-title">
 					Каталог <br> скачать pdf
 				</h6>
 			</a>
-			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="<?php echo home_url(); ?>/souvenirs/">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/souvenir.png" alt="" class="distributors-img">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/souvenir.png" class="distributors-img-hover" />
-				<h6 class="distributors-title">
-					Рекламно - сувенирная <br> продукция
-				</h6>
-			</a>
-			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="<?php echo home_url(); ?>/trading-equipment/">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/equipment.png" alt="" class="distributors-img">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/equipment.png" class="distributors-img-hover" />
-				<h6 class="distributors-title">
-					Торговое <br> оборудование
-				</h6>
-			</a>
-			<a class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3 distributors-item d-flex justify-content-around align-items-center flex-column text-center" href="<?php echo home_url(); ?>/sales">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/distributors/stock.png" alt="" class="distributors-img-hover" />
-                <img src="<?php echo get_template_directory_uri(); ?>/img/distributors/stock.png" alt="" class="distributors-img" />
-                <h6 class="distributors-title">
-					Акции
-				</h6>
-			</a>
+
+            <?php }; endif;?>
+
 		</div>
 	</div>
 </main>
