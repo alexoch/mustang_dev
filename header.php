60c67101c566b77117109908098933d7516c8145
DESKTOP-NAETRR5\soche <s.ocheretyany@gmail.com>
2020-01-30 09:30:24 +0200

    updating menu3

--- header.php
+++ header.php
@@ -32,6 +32,8 @@
             <ul class="navbar-nav ml-auto">
 	            <?php
 	            $menu_name = "Header-menu";
+	            $locations = get_nav_menu_locations();
+	            var_dump($locations);
 	            if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
 		            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
 		            $menu_items = wp_get_nav_menu_items($menu->term_id );
